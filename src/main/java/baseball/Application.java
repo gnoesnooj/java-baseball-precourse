c2031014440f2c1958d6c1da1eb8fc19e4214ddd
gnoesnooj <306yyy@naver.com>
2021-12-09 22:27:40 +0900

    check

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
+
+    public void checkBallCount(){
+
+    }
+
+    public void makeNumber(){
+
+    }
+
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        Scanner sc = new Scanner(System.in);
     }
 }
